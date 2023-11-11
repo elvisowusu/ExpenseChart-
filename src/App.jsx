b1201c93de14ad4e0df71ab3a76ddfb807ebb004
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-11 23:13:59 +0000

    setting up bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BsFillCircleFill } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col items-center">
       <div>
         <div className="flex">
           <p>My balance</p>
@@ -16,14 +16,14 @@ function App() {
       </div>
       <div>
         <h1>Spending - Last 7 days</h1>
-        <div>
-          <div className=""></div>
-          <div className=""></div>
-          <div className=""></div>
-          <div className=""></div>
-          <div className=""></div>
-          <div className=""></div>
-          <div className=""></div>
+        <div className="flex">
+          <div className="">1</div>
+          <div className="">2</div>
+          <div className="">3</div>
+          <div className="">4</div>
+          <div className="">5</div>
+          <div className="">6</div>
+          <div className="">7</div>
         </div>
       </div>
     </div>
