6a2443537cb30a1e177477856b1f18f70f28988f
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-12 01:59:31 +0000

    Update styling for balance and spending sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BsFillCircleFill } from "react-icons/bs";
 function App() {
   return (
     <div className="flex flex-col md:justify-center items-center md:h-[100vh] bg-Cream font-DMsans">
-      <div className="flex justify-between items-center bg-Softred rounded-lg p-5 w-[22rem]">
+      <div className="flex justify-between items-center bg-Softred rounded-lg p-6 w-[22rem]">
         <div>
           <p className="text-Cream">My balance</p>
           <p className="text-white font-bold text-[1.5rem]">$921.48</p>
@@ -14,16 +14,16 @@ function App() {
           <BsFillCircleFill className="h-[2.7rem] w-[2.7rem]"/>
         </div>
       </div>
-      <div className="rounded-lg bg-white p-5 w-[22rem]">
-        <h1 className="text-Darkbrown">Spending - Last 7 days</h1>
-        <div className="flex justify-evenly i">
-          <div className="bg-Softred hover:bg-Verypaleorange">mon</div>
-          <div className="bg-Softred">tue</div>
-          <div className="bg-Cyan">wed</div>
-          <div className="bg-Softred">thu</div>
-          <div className="bg-Softred">fri</div>
-          <div className="bg-Softred">sat</div>
-          <div className="bg-Softred">sun</div>
+      <div className="rounded-lg bg-white p-6 w-[22rem] mt-4 py-6">
+        <h1 className="text-Darkbrown font-bold text-[1.5rem]">Spending - Last 7 days</h1>
+        <div className="flex justify-evenly gap-[0.7rem]">
+          <div className="bg-Softred hover:bg-Verypaleorange w-[2.3rem] h-4"></div>
+          <div className="bg-Softred w-[2.3rem]"></div>
+          <div className="bg-Cyan w-[2.3rem]"></div>
+          <div className="bg-Softred w-[2.3rem]"></div>
+          <div className="bg-Softred w-[2.3rem]"></div>
+          <div className="bg-Softred w-[2.3rem]"></div>
+          <div className="bg-Softred w-[2.3rem]"></div>
         </div>
         <div className="flex justify-evenly"><p className="text-Mediumbrown">mon</p> <p className="text-Mediumbrown">tue</p> <p className="text-Mediumbrown">thu</p> <p className="text-Mediumbrown">fri</p> <p className="text-Mediumbrown">sat</p> <p className="text-Mediumbrown">sun</p></div>
         <hr className="border-none h-[0.04rem] bg-Cream"/>
