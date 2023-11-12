f10e050e227f7acc9021802ee919e054771a59eb
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-12 15:56:38 +0000

    added more styles for lg screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BsFillCircleFill } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-[110vh] md:h-[100vh] bg-Cream font-DMsans">
-      <div className="flex justify-between items-center bg-Softred rounded-lg p-6 w-[23rem]">
+    <div className="flex flex-col justify-center items-center h-[110vh] md:h-[100vh] bg-Cream font-DMsans lg:bg-red-300 xl:bg-blue-200">
+      <div className="flex justify-between items-center bg-Softred rounded-lg p-6 lg:h-[5.5rem] lg:rounded-xl w-[23rem] lg:w-[30rem]">
         <div>
-          <p className="text-Cream">My balance</p>
+          <p className="text-Cream text-sm">My balance</p>
           <p className="text-white font-bold text-[1.5rem]">$921.48</p>
         </div>
         <div className="relative flex justify-center">
@@ -14,31 +14,31 @@ function App() {
           <BsFillCircleFill className="h-[2.7rem] w-[2.7rem]"/>
         </div>
       </div>
-      <div className="rounded-lg bg-white p-6 w-[23rem] mt-4 py-6">
-        <h1 className="text-Darkbrown font-bold text-[1.5rem]">Spending - Last 7 days</h1>
-        <div className="flex justify-evenly gap-[0.9rem] items-baseline mt-[3.9rem]">
+      <div className="rounded-lg lg:rounded-xl bg-white p-6 w-[23rem] lg:w-[30rem] mt-4 py-6">
+        <h1 className="text-Darkbrown font-bold text-[1.5rem] lg:text-[1.9rem]">Spending - Last 7 days</h1>
+        <div className="flex justify-evenly gap-[0.9rem] items-baseline mt-[3.9rem] lg:mt-[3.5rem]">
           <div className="flex flex-col items-center gap-2">
-            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[3.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm">mon</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] lg:w-[2.7rem] h-[3.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm">mon</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[7.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >tue</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] lg:w-[2.7rem] h-[7.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >tue</p>
           </div>
           <div className="flex flex-col items-center gap-2 relative">
-            <p className="peer/wed bg-Cyan hover:bg-  w-[2.1rem] h-[13rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >wed</p>
-            <p className="bg-Darkbrown invisible peer-hover/wed:visible text-sm text-white p-[0.4rem] rounded-md absolute bottom-[15.3rem]">$52.36</p>
+            <p className="peer/wed bg-Cyan hover:bg-  w-[2.1rem] lg:w-[2.7rem] h-[13rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >wed</p>
+            <p className="bg-Darkbrown invisible peer-hover/wed:visible text-sm text-white p-[0.4rem] lg:p-[0.5rem] rounded-md absolute bottom-[15.3rem]">$52.36</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="peer/thu bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[6.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >thu</p>
-            <p className="peer-hover/thu:visible invisible bg-Darkbrown text-sm text-white p-[0.4rem] rounded-md absolute bottom-[22.7rem]">$31.07</p>
+            <p className="peer/thu bg-Softred hover:bg-Verypaleorange w-[2.1rem] lg:w-[2.7rem] h-[6.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >thu</p>
+            <p className="peer-hover/thu:visible invisible bg-Darkbrown text-sm text-white p-[0.4rem] lg:p-[0.5rem] rounded-md absolute bottom-[22.9rem]">$31.07</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[4.9rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >fri</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] lg:w-[2.7rem] h-[4.9rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >fri</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[9.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sat</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] lg:w-[2.7rem] h-[9.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sat</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[5.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sun</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] lg:w-[2.7rem] h-[5.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sun</p>
           </div>
         </div>
         <hr className="border-none h-[0.1rem] bg-Cream my-[1.6rem]"/>
