a7b0b6a3701572cbbd61e2f542149591bcd59e18
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-12 15:10:05 +0000

    added new text

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,31 @@ function App() {
         <h1 className="text-Darkbrown font-bold text-[1.5rem]">Spending - Last 7 days</h1>
         <div className="flex justify-evenly gap-[0.7rem] items-baseline mt-[3.9rem]">
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Softred hover:bg- w-[2.1rem] h-[3.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm">mon</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[3.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm">mon</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[7.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >tue</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[7.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >tue</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Cyan hover:bg- w-[2.1rem] h-[13rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >wed</p>
+            <p className="bg-Darkbrown text-sm text-white p-[0.4rem] rounded-md">$52.36</p>
+            <p className="bg-Cyan hover:bg- w-[2.1rem] h-[13rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >wed</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[6.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >thu</p>
+          <p className="bg-Darkbrown text-sm text-white p-[0.4rem] rounded-md">$31.07</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[6.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >thu</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[4.9rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >fri</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[4.9rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >fri</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[9.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sat</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[9.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sat</p>
           </div>
           <div className="flex flex-col items-center gap-2">
-          <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[5.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sun</p>
+            <p className="bg-Softred hover:bg-Verypaleorange w-[2.1rem] h-[5.6rem] rounded-sm cursor-pointer"></p><p className="text-Mediumbrown text-sm" >sun</p>
           </div>
         </div>
         <hr className="border-none h-[0.1rem] bg-Cream my-[1.6rem]"/>
-        <p className="text-Mediumbrown">Total this month</p>
+          <p className="text-Mediumbrown">Total this month</p>
         <div className="flex justify-between items-center">
           <p className="text-Darkbrown font-bold text-[1.95rem]">$478.33</p> <div className="flex flex-col items-end"><p className="text-Darkbrown font-bold">+2.4%</p><p className="text-Mediumbrown">from last month</p></div></div>
       </div>
