7e762c8a7d1bec278ebc1735a171d5970a58b2f2
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-12 00:18:22 +0000

    added more styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,33 @@ import { BsFillCircleFill } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center md:h-[100vh]">
+    <div className="flex flex-col justify-center items-center h-[100vh] bg-Cream">
       <div className="flex items-center bg-Softred">
         <div>
-          <p className="">My balance</p>
-          <p className="">$921.48</p>
+          <p className="text-">My balance</p>
+          <p className="text-white">$921.48</p>
         </div>
         <div className="relative flex justify-center">
           <BsCircle className="text-white h-[1.8rem] w-[2rem]"/>
           <BsFillCircleFill className="h-[1.8rem] w-[2rem]"/>
         </div>
       </div>
-      <div>
-        <h1>Spending - Last 7 days</h1>
+      <div className="bg-white">
+        <h1 className="text-Darkbrown">Spending - Last 7 days</h1>
         <div className="flex justify-evenly">
-          <div className="">mon</div>
-          <div className="">tue</div>
-          <div className="">wed</div>
-          <div className="">thu</div>
-          <div className="">fri</div>
+          <div className="bg-Softred hover:bg-Verypaleorange">mon</div>
+          <div className="bg-Softred">tue</div>
+          <div className="bg-Cyan">wed</div>
+          <div className="bg-Softred">thu</div>
+          <div className="bg-Softred">fri</div>
           <div className="">sat</div>
-          <div className="">sun</div>
+          <div className="bg-Softred">sun</div>
         </div>
         <div className="flex justify-evenly"><p>mon</p> <p>tue</p> <p>thu</p> <p>fri</p> <p>sat</p> <p>sun</p></div>
         <hr className="border-none h-[0.04rem] bg-Cream"/>
+        <p className="text-Mediumbrown">Total this month</p>
+        <div>
+          <p className="text-Darkbrown">$478.33</p> <div><p className="text-Darkbrown">+2.4%</p><p className="text-Mediumbrown">from last month</p></div></div>
       </div>
     </div>
   );
